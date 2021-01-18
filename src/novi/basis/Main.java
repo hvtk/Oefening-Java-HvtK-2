a31d980b7d135b3ebe7af34117ddd7de950335a0
hvtk <hvtk1968@gmail.com>
2021-01-18 09:15:21 +0100

    second setup

--- src/novi/basis/Main.java
+++ src/novi/basis/Main.java
@@ -3,6 +3,7 @@ package novi.basis;
 public class Main {
     private String name;                //class attribute
     private String abbreviation;        //class attribute
+    private int foundingYear;
 
     public String getName() {
         return name;
@@ -10,6 +11,10 @@ public class Main {
 
     public String getAbbreviation() {
         return abbreviation;
+    }
+
+    public int getFoundingYear() {
+        return foundingYear;
     }
 
     public void setAbbreviation(String abbreviation) {  // staat hier String.. tussen () omdat deze methode ook een andere variabele mee kan geven?
@@ -20,14 +25,20 @@ public class Main {
         this.name = name;
     }
 
-    public Main(String name, String abbreviation) {
-        this.name = name;
-        this.abbreviation = abbreviation;
+    public void setFoundingYear(int foundingYear) {
+        this.foundingYear = foundingYear;
     }
 
-    public static void main(String[] args) {
-       // Main ajax = new Main("Ajax Amsterdam", "AFCA");   //create an object in main ajax is het object, name en abbrecviation zijn de instance variable
-        Main ajax = new Main("Ajax Amsterdam", "AFCA");
-        System.out.println("voetbalclub " + ajax.getName() + " met de afkorting " + ajax.getAbbreviation());
+    // This is the constructor
+    public Main(String name, String abbreviation, int foundingYear) {
+        this.name = name;
+        this.abbreviation = abbreviation;
+        this.foundingYear = foundingYear;
+    }
+    //create an object in main ajax is het object, name en abbrecviation zijn de instance variable
+    public static void main(String[] args) {  //Waarom wordt hier ook int geaccepteert omdat er alleen maar String[] staat
+        Main ajax = new Main("Ajax Amsterdam", "AFCA", 1902); // Ajax Amsterdam en AFCA zijn de instance variabelen
+        System.out.println("voetbalclub " + ajax.getName() + " met de afkorting " + ajax.getAbbreviation() + " is opgericht in " + ajax.getFoundingYear());
     }
 }
+
